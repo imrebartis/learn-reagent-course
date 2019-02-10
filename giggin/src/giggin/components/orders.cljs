d9be05cc4951c3f0a70a1fb67a8b86cded1d3dc9
Imre Bartis <imre.bartis@siili.com>
2019-02-10 17:53:33 +0200

    allow users to remove orders

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -16,4 +16,8 @@
               [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
              [:div.action
               ; multiply price with quant
-              [:div.price (* (get-in @state/gigs [id :price]) quant)]]])]]])
+              [:div.price (* (get-in @state/gigs [id :price]) quant)]
+              [:button.btn.btn--link.tooltip
+                {:data-tooltip "Remove"
+                 :on-click (fn [] (swap! state/orders dissoc id))}
+                 [:i.icon.icon--cross]]]])]]])
