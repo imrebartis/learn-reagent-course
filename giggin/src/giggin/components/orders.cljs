fa446a44dcb891283376d72aa1e5c8db3b4b4921
Imre Bartis <imre.bartis@siili.com>
2019-03-11 14:09:06 +0200

    fix indentation in orders.cljs; use let binding in gigs.cljs, too

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -28,28 +28,28 @@
   []
   (let [remove-from-order #(swap! state/orders dissoc %)
         remove-all-orders #(reset! state/orders {})]
-  [:aside
-    (if (empty? @state/orders)
-      [:div.empty
-        [:div.title "You don't have any orders"]
-        [:div.subtitle "Click on a + to add an order"]])
-      [:div.order
-        [:div.body
-          (for [[id quant] @state/orders]
-            [:div.item {:key id}
-              [:div.img
-                [:img {:src (get-in @state/gigs [id :img])
-                       :alt (get-in @state/gigs [id :title])}]]
-               [:div.content
-               ; string concatenation
-                [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
-               [:div.action
-                ; multiply price with quant
-                [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
-                [:button.btn.btn--link.tooltip
-                  {:data-tooltip "Remove"
-                   :on-click #(remove-from-order id)}
-                   [:i.icon.icon--cross]]]])]
+    [:aside
+      (if (empty? @state/orders)
+        [:div.empty
+          [:div.title "You don't have any orders"]
+          [:div.subtitle "Click on a + to add an order"]])
+        [:div.order
+          [:div.body
+            (for [[id quant] @state/orders]
+              [:div.item {:key id}
+                [:div.img
+                  [:img {:src (get-in @state/gigs [id :img])
+                         :alt (get-in @state/gigs [id :title])}]]
+                 [:div.content
+                 ; string concatenation
+                  [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+                 [:div.action
+                  ; multiply price with quant
+                  [:div.price (format-price (* (get-in @state/gigs [id :price]) quant))]
+                  [:button.btn.btn--link.tooltip
+                    {:data-tooltip "Remove"
+                     :on-click #(remove-from-order id)}
+                     [:i.icon.icon--cross]]]])]
           [:div.total
             [:hr]
             [:div.item
@@ -59,7 +59,6 @@
                 [:div.price (format-price (total))]]
               [:button.btn.btn--link.tooltip
                 {:data-tooltip "Remove all"
-                ; the {} below is an empty map
                  :on-click #(remove-all-orders)}
                 [:i.icon.icon--delete]]]
         ]]]))
