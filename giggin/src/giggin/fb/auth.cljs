f1645b3d79a124419fe184c6bbbee31920fcb6a0
Imre Bartis <imre.bartis@siili.com>
2019-03-27 15:29:49 +0200

    wire up firebase auth

---
+++ giggin/src/giggin/fb/auth.cljs
@@ -0,0 +1,36 @@
+(ns giggin.fb.auth
+  (:require ["firebase/app" :as firebase]
+            [giggin.fb.db :refer [db-save!]]))
+
+  ;; from https://firebase.google.com/docs/auth/web/google-signin?authuser=0
+  ;; var provider = new firebase.auth.GoogleAuthProvider();
+  (defn sign-in-with-google
+    []
+    (let [provider (firebase/auth.GoogleAuthProvider.)] ;; the '.' in the end stands for 'new' in JS
+    ;; firebase.auth().signInWithPopup(provider)
+      (.signInWithPopup (firebase/auth) provider)))
+
+  ;; firebase.auth().signOut()
+  (defn sign-out
+    []
+    (.signOut (firebase/auth)))
+
+  ;; from https://firebase.google.com/docs/auth/web/manage-users?authuser=0
+  ;; firebase.auth().onAuthStateChanged(function(user)
+  (defn on-auth-state-changed
+  []
+  (.onAuthStateChanged
+   (firebase/auth)
+   (fn
+     [user]
+     (when user
+       (let [uid (.-uid user)
+             display-name (.-displayName user)
+             photo-url (.-photoURL user)
+             email (.-email user)]
+          (db-save!
+           ["users" uid "profile"]
+           #js {:photo-url photo-url
+                 :display-name display-name
+                 :email email}
+           ))))))
